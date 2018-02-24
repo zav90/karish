c893093fb894fc3541d2463c7ac6352a0ac2b862
iliya <arcliya@gmail.com>
2018-02-24 22:43:03 +0200

    edite

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello from Flask suka !'
 
 
 if __name__ == '__main__':
