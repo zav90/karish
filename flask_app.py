d025c2b39529afe697189a0b4314e6caaf49e9fe
iliya <arcliya@gmail.com>
2018-02-25 20:59:28 +0200

    edite

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,13 @@
 # A very simple Flask Hello World app for you to get started with...
-
+import sqlalchemy
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{zav90}:{password}@{hostname}/{databasename}".format(
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="zav90",
     password="1qazxsw@",
     hostname="zav90.mysql.pythonanywhere-services.com",
@@ -17,6 +18,13 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 @app.route('/')
 def hello_world():
