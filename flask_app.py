0a1d41226fdc6ba2fc094111a09b4d30d7f0ec6b
iliya <arcliya@gmail.com>
2018-02-25 18:32:56 +0000

    modified flask_app

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,16 @@ from flask import Flask
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="zav90",
+    password="1qazxsw@",
+    hostname="zav90.mysql.pythonanywhere-services.com",
+    databasename="zav90$ZavDB",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 @app.route('/')
 def hello_world():
     return 'Hello from Flask suka !'
